71cc36ec269e872729b2dc263c95d5022c8e451e
Guilherme Gomes <guilherme09396@gmail.com>
2025-06-28 15:17:01 -0300

    Deixando opcional a relação entre links e usuários

---
+++ prisma/migrations/20250628181325_relation_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "links" ALTER COLUMN "user_id" DROP DEFAULT;
--- src/repositories/link-repository.ts
+++ src/repositories/link-repository.ts
@@ -1,6 +1,6 @@
 import { Links, Prisma } from 'generated/prisma'
 
 export interface LinkRepository {
-  create(data: Prisma.LinksCreateInput): Promise<Links>
+  create(data: Prisma.LinksUncheckedCreateInput): Promise<Links>
   findBySlug(slug: string): Promise<Links | null>
 }
