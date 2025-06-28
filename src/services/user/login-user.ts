982d3f56bfd86a02387054063b380141163b44df
Guilherme Gomes <guilherme09396@gmail.com>
2025-06-28 14:58:07 -0300

    Serviço de login de usuário criado

---
+++ src/services/user/login-user.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from '@/repositories/user-repository'
+import { compare } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { CredentialsInvalidError } from '../errors/credentials-invalid-error'
+import { env } from '@/env'
+
+interface LoginUserRequest {
+  email: string
+  password_hash: string
+}
+interface LoginUserResponse {
+  token: string
+}
+
+export class LoginUser {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({
+    email,
+    password_hash,
+  }: LoginUserRequest): Promise<LoginUserResponse> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      throw new CredentialsInvalidError()
+    }
+
+    const passwordIsCorrect = await compare(password_hash, user.password_hash)
+    if (!passwordIsCorrect) {
+      throw new CredentialsInvalidError()
+    }
+
+    const token = jwt.sign(user.id, env.JWT_SECRET)
+    return { token }
+  }
+}
