f2315e298d2a9233a37fd357b937ec0399f56945
Guilherme Gomes <guilherme09396@gmail.com>
2025-06-28 14:01:40 -0300

    Middleware criado tanto para verificar se o usuário está logado e restringir acesso somente a usuário logado

---
+++ src/middlewares/verify-user-logged.ts
@@ -0,0 +1,37 @@
+import { env } from '@/env'
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+
+export async function verifyUserLogged(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const headers = req.headers.authorization
+    req.userId = undefined
+    if (headers) {
+      const [_, token] = headers.split(' ')
+      req.userId = jwt.verify(token, env.JWT_SECRET) as string
+    }
+  } catch (e) {
+    console.error(e)
+  } finally {
+    next()
+  }
+}
+
+export async function restrictedAccessLoggedInUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { userId } = req
+    if (userId) {
+      next()
+    } else {
+      res.status(400).json({ error: 'Access restrict' })
+    }
+  } catch (e) {}
+}
