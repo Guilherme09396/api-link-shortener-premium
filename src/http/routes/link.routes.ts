8c54fdaaa92629f4553cf4c1a7292e5c1a2cf50f
Guilherme Gomes <guilherme09396@gmail.com>
2025-06-28 14:45:43 -0300

    Criação das rota de criação de usuário e modificação na estrutura das rotas

--- src/http/routes/link.routes.ts
+++ src/http/routes/link.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createShortenedLink } from './controllers/link/create-shortened-link'
-import { findByLinkBySlug } from './controllers/link/find-by-link-by-slug'
 import {
   blocksLinkCreationPerDay,
   blocksLinkCreationPerMinute,
 } from '@/middlewares/verify-client-access-by-ip'
 import { verifyUserLogged } from '@/middlewares/verify-user-logged'
+import { createShortenedLink } from '../controllers/link/create-shortened-link'
+import { findByLinkBySlug } from '../controllers/link/find-by-link-by-slug'
 const router = express.Router()
 
 router.use(verifyUserLogged)
